626a18425e79e651a134bf8714bd54ffdef57f19
jumpt57 <jumpt57@github.com>
2017-01-13 13:35:47 +0100

    First commit.

---
+++ app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { MainComponent } from './main.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    MainComponent
+  ],
+  providers: [
+  ],
+  bootstrap: [
+    MainComponent
+  ]
+})
+export class AppModule { }
